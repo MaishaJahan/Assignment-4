8fd2eb45ada47af88d4d353624e7ef9f3ff9f029
MaishaJahan <60590764+MaishaJahan@users.noreply.github.com>
2020-03-28 16:26:53 -0400

    Add files via upload

---
+++ ManagementCompany.java
@@ -0,0 +1,137 @@
+public class ManagementCompany {
+
+  private final int MAX_PROPERTY = 5, MAX_WIDTH = 10, MAX_DEPTH = 10;
+  private String name, taxID;
+  private double mgmFeePer;
+  private Property[] properties;
+  private Plot plot;
+  
+  public int getMAX_PROPERTY() {
+	    return MAX_PROPERTY;
+	  }
+
+  public Plot getPlot() {
+    return plot;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public int maxRentPropertyIndex() {
+	int index = 0;
+	for (int i = 0; i < properties.length; i++) {
+	  index += 1;
+	    }
+	return index;
+  }
+  
+  public String maxRentProp() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+  
+  public ManagementCompany() {
+    this.name = "";
+    this.taxID = "";
+    this.mgmFeePer = 0;
+    this.plot = new Plot();
+    this.properties = new Property[MAX_PROPERTY];
+  }
+
+  public ManagementCompany(String name, String taxID, double mgmFeePer) {
+    this.name = name;
+    this.taxID = taxID;
+    this.mgmFeePer = mgmFeePer;
+    this.plot = new Plot(0,0,MAX_WIDTH, MAX_DEPTH);
+    this.properties = new Property[MAX_PROPERTY];
+  }
+
+  public ManagementCompany(String name, String taxID, double mgmFeePer, int x, int y, int width, int depth) {
+    this.name = name;
+    this.taxID = taxID;
+    this.mgmFeePer = mgmFeePer;
+    this.plot = new Plot(x,y,width,depth);
+    this.properties = new Property[MAX_PROPERTY];
+  }
+
+  public int addProperty(Property property) {
+    if (property == null) {
+      return -2;
+    }
+    
+    if (!plot.encompasses(property.getPlot())) {
+      System.out.println(plot.getWidth() + " " + plot.getDepth());
+      System.out.println(property.getPlot().getWidth() + " " + property.getPlot().getDepth());
+      return -3;
+    }
+    
+    for (int i = 0;i < properties.length;i++) {
+      if (properties[i] != null) {
+        if (properties[i].getPlot().overlaps(property.getPlot())) {
+          return -4;
+        }
+      } else {
+        properties[i]=property;
+        return i;
+      } 
+    }
+      return -1;
+  }
+  
+  public int addProperty(String propertyName, String city, double rent, String ownerName) {
+    return addProperty(new Property(propertyName, city, rent, ownerName));
+  }
+  
+  public int addProperty(String propertyName, String city, double rent, String ownerName, int x, int y, int width, int depth) {
+    return addProperty(new Property(propertyName, city, rent, ownerName, x, y, width, depth));
+  }
+  
+  public double totalRent() {
+	double total = 0;
+	for (int i=0;i<properties.length;i++) {
+	  if (properties[i]==null) {
+	     break;
+	  }
+	  total += properties[i].getRentAmount();
+	  }
+	 return total;
+  }
+	  
+  private int maxPropertyRentIndex() {
+	int index = -1;
+	double max = 0;
+	for (int i = 0;i<properties.length;i++) {
+	   if (properties[i]==null) {
+	      break;
+	   }
+	   if (properties[i].getRentAmount() > max) {
+	      max = properties[i].getRentAmount();
+	      index = i;
+	   }
+	 }
+	return index;
+  }
+  
+  public String maxPropertyRent() {
+    return properties[maxPropertyRentIndex()].toString();
+  }
+  
+  String displayPropertyAtIndex(int index) {
+    return properties[index].toString();
+  }
+  
+  public String toString() {
+
+    String listOfProperties = "";
+    for (int i = 0; i < MAX_PROPERTY; i++) {
+      if (properties[i]==null) {
+        break;
+      } 
+      listOfProperties += properties[i] + ", "; 
+    }
+    return "List of the properties for: " + name + "\n" + "TaxID: " + taxID + "\n" + listOfProperties +
+    		"\n" + "Total management Fee: " + (totalRent() * mgmFeePer / 100);
+  }
+
+}
---
+++ Plot.java
@@ -0,0 +1,86 @@
+public class Plot {
+  private int x;
+  private int y;
+  private int width;
+  private int depth;
+
+  public Plot() {
+    this.x = 0;
+    this.y = 0;
+    this.width = 1;
+    this.depth = 1;
+  }
+
+  public Plot(Plot p) {
+    this.x = p.x;
+    this.y = p.y;
+    this.width = p.width;
+    this.depth = p.depth;
+  }
+
+  public Plot(int x, int y, int width, int depth) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.depth = depth;
+  }
+
+  public int getX() {
+    return x;
+  }
+
+  public void setX(int x) {
+    this.x = x;
+  }
+
+  public int getY() {
+    return y;
+  }
+
+  public void setY(int y) {
+    this.y = y;
+  }
+
+  public int getWidth() {
+    return width;
+  }
+
+  public void setWidth(int width) {
+    this.width = width;
+  }
+
+  public int getDepth() {
+    return depth;
+  }
+
+  public void setDepth(int depth) {
+    this.depth = depth;
+  }
+  
+  public boolean encompasses(Plot p) {
+    boolean goodX = p.x >= x;
+    boolean goodY = p.y >= y;
+    boolean goodWidth = (p.x + p.width) <= (x + width);
+    boolean goodDepth = (p.y + p.depth) <= (y + depth);
+    
+    return goodX && goodY && goodWidth && goodDepth;
+  }
+  
+  public boolean overlaps(Plot p) {
+    boolean aXY1 = (p.x >= x && p.x < (x + width)) &&  (p.y >= y && p.y < (y + depth));
+    boolean aXY2 = (x >= p.x && x < (p.x + width)) &&  (y >= p.y && y < (p.y + p.depth));
+    boolean bXY1 = (p.x + p.width) > x && (p.x + p.width) < (x + width) && p.y >= y && p.y <= (y + depth); 
+    boolean bXY2 = (x + width) > p.x && (x + width) < (p.x + p.width) && y >= p.y && y <= (p.y + p.depth); 
+    boolean cXY1 = (p.x >= x )&& p.x < (x +  width) && (p.y + p.depth) > y && (p.y + p.depth) <= (y + depth);
+    boolean cXY2 = (x >= p.x )&& (x < (p.x +  p.width) )&& (y + depth) > p.y && (y + depth) <= (p.y + p.depth);
+    boolean dXY1 = (p.x + p.width) > x && (p.x +p.width) <= (x + width) && (p.y + p.depth) > y && (p.y + p.depth) <= (y + depth);
+    boolean dXY2 = (x + width) > p.x && (x + width) <= (p.x + p.width) && (y + depth) > p.y && (y + depth) <= (p.y + p.depth);
+    
+    return aXY1 || aXY2 || bXY1 || bXY2 || cXY1 || cXY2 || dXY1 || dXY2;
+  }
+  
+  @Override
+  public String toString() {
+    return "Upper left: (" + x + "," + y + "); Width: " + width + " Depth: " + depth;
+  }
+}
